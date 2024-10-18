7d55a6849bbed432f211ce7827081fa4597f7f27
rsbrennan-NOAA <reid.brennan@noaa.gov>
2024-10-18 16:28:25 -0700

    summary

--- scripts/population_assignment_DAPC.R
+++ scripts/population_assignment_DAPC.R
@@ -9,15 +9,12 @@ dat <- read.structure(
   onerowperind = FALSE,
   n.loc=6479 ,
   n.ind=136 ,
-  col.lab = NULL,
+  col.lab = 0,
   col.pop = 2,
   NA.char = "-9",
   col.others=1,
   row.marknames=1
 )
-
-
-
 
 genl <- gi2gl(dat, parallel = FALSE, verbose = NULL)
 genl@ind.names <- dat@other$X
@@ -26,13 +23,13 @@ genl@ind.names <- dat@other$X
 
 pca1 <- glPca(genl,center = T, scale = T, nf = 5)
 
-#png("dapc_eigen.png", h=4, w=4, units="in", res=300)
+png("figures/pca_eigen.png", h=4, w=4, units="in", res=300)
 barplot(100*pca1$eig/sum(pca1$eig), col = heat.colors(50), main="PCA Eigenvalues") # retain first 5 axes, incremental decrease after 2
 title(ylab="Percent of variance\nexplained", line = 2)
 title(xlab="Eigenvalues", line = 1)
-#dev.off()
+dev.off()
 
-#proportion of explained variance by first three axes
+#proportion of explained variance by first four axes
 a1<-pca1$eig[1]/sum(pca1$eig) # proportion of variation explained by 1st axis
 a2<-pca1$eig[2]/sum(pca1$eig) # proportion of variation explained by 2nd axis 
 a3<-pca1$eig[3]/sum(pca1$eig) # proportion of variation explained by 3rd axis
@@ -48,20 +45,16 @@ set.seed(89)
 num_pops <- length(levels(factor(pca1.scores$pop)))
 
 # plot PC 1 and 2
-pca1.p<-ggscatter(pca1.scores, x = "PC1", y = "PC2", color = "pop",
-                  ellipse = T, ellipse.level = 0.95, size = 3,
-                  xlab = paste0("PC1 (",round(pcvar[1,2]*100,2),"%)"),
-                  ylab = paste0("PC2 (",round(pcvar[2,2]*100,2),"%)")
-)
-pca1.p
-#ggsave("dapc_PCA.png",pca1.p, h=5, w=5)
-pca2.p<-ggscatter(pca1.scores, x = "PC2", y = "PC3", color = "pop",
-                  ellipse = T, ellipse.level = 0.95,
-                  xlab = paste0("PC2 (",round(pcvar[2,2]*100,2),"%)"),
-                  ylab = paste0("PC3 (",round(pcvar[3,2]*100,2),"%)"))
-pca2.p
 
+p1 <- ggplot(pca1.scores, aes(x=PC1, y=PC2, fill=pop))+
+  geom_point(size=4, pch=21)+
+  xlab(paste0("PC1 (",round(pcvar[1,2]*100,2),"%)") )+
+  ylab(paste0("PC2 (",round(pcvar[2,2]*100,2),"%)")) + 
+  theme_bw(base_size = 14) +
+  scale_fill_manual(values=c("orange3", "red3", "darkgreen", "lawngreen"))
+p1
 
+ggsave("figures/pca.png", p1, h=4, w=6)
 
 ############ ----------------------------------------------------------------
 
@@ -97,7 +90,7 @@ scatter(dapc2)
 ###### --------------------------------------------------------------------------
 # leave one out, assignment success
 
-# leave out ~30% of samples.
+# leave out 30% of samples.
 kept.id <- sample(1:nInd(genl), size=round(nInd(genl)*0.7), replace=F)
 
 x <- genl[kept.id,]
@@ -141,7 +134,7 @@ for(i in 1:n){
   # leave out ~20% of samples.
   kept.id <- sample(1:nInd(genl), size=round(nInd(genl)*0.8), replace=F)
   x <- genl[kept.id,]
-  x.sup <- genl[!1:nInd(genl) %in% kept.id,]
+  x.sup <- genl[!(1:nInd(genl)) %in% kept.id,]
   # run dapc
   dapc4 <- dapc(x , x@pop,n.pca=4,n.da=5)
   pred.sup <- predict.dapc(dapc4, newdata=x.sup )
@@ -170,12 +163,108 @@ for(i in 1:n){
   print(i)
 }
 
-boxplot(acc.out.4)
+write.csv(file="analysis/dapc_accuracy_prop.csv", df.out.4, row.names=F)
 
-head(df.out.4)
+library(tidyr)
 
-boxplot(df.out.4)
+df_long <- df.out.4 %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = "population",
+    values_to = "accuracy"
+  )
 
-png(filename = "./figures/dapc.accuracyPops.png", h=6, w=8, units="in", res=300)
-boxplot(df.out.4, ylab="Proportion correct assignment")
-dev.off()
+
+p1 <- ggplot(df_long, aes(x=population, y=accuracy, fill=population))+
+  geom_boxplot()+
+  ylab("% correct assignments") +
+  xlab("population") + 
+  theme_bw(base_size = 14) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+        legend.position="none") +
+  ylim(0,1) +
+  scale_fill_manual(values=c("darkgreen", "lawngreen", "orange3", "red3")) +
+  ggtitle("DAPC: all individuals")
+
+ggsave("figures/dapc_allindivs_accuracy.png",p1,
+       h=4, w=5)
+
+
+
+#-------------------------------------------------------------------------------
+# downsample to 14 indivs, leave out 4 indivs
+
+# loop to get some estimate of accuracy
+n=100
+acc.out.4 <- rep(NA, n)
+df.out.fixed <- as.data.frame(matrix(nrow=n, ncol=4))
+colnames(df.out.fixed) <- c("Orange_84", "Red_21", "DarkGreen_17", "LightGreen_14")
+
+orange <- which(genl@pop == "Orange_84")
+Lgreen <- which(genl@pop == "LightGreen_14")
+Dgreen <- which(genl@pop == "DarkGreen_17")
+red   <- which(genl@pop == "Red_21")
+sampN <- 10
+
+for(i in 1:n){
+
+  orange.in <- sample(orange, sampN, replace=F)
+  Lgreen.in <- sample(Lgreen, sampN, replace=F)
+  Dgreen.in <- sample(Dgreen, sampN, replace=F)
+  red.in    <- sample(red, sampN, replace=F)
+  kept.id <- c(orange.in,Lgreen.in,Dgreen.in,red.in )
+  x.sup <- genl[!(1:nInd(genl)) %in% kept.id,]
+  x <- genl[kept.id,]
+  
+  # run dapc
+  dapc4 <- dapc(x , x@pop,n.pca=4,n.da=5)
+  pred.sup <- predict.dapc(dapc4, newdata=x.sup )
+  
+  # calculate accuracy of assignments. i.e., is anything coming from its actual population
+  acc.out.4[i] <- mean(as.character(pred.sup$assign)==as.character(pop(x.sup)))
+  
+  # get accuracy by population
+  #
+  orange.tmp <- which(as.character(pop(x.sup)) == "Orange_84")
+  if(length(orange.tmp) > 0){
+    df.out.fixed$Orange_84[i] <- mean(as.character(pred.sup$assign[orange.tmp])==as.character(pop(x.sup[orange.tmp])))
+  }
+  red.tmp <- which(as.character(pop(x.sup)) == "Red_21")
+  if(length(red.tmp) > 0){
+    df.out.fixed$Red_21[i] <- mean(as.character(pred.sup$assign[red.tmp])==as.character(pop(x.sup[red.tmp])))
+  }
+  darkgreen.tmp <- which(as.character(pop(x.sup)) == "DarkGreen_17")
+  if(length(darkgreen.tmp) > 0){
+    df.out.fixed$DarkGreen_17[i] <- mean(as.character(pred.sup$assign[darkgreen.tmp])==as.character(pop(x.sup[darkgreen.tmp])))
+  }
+  lightgreen.tmp <- which(as.character(pop(x.sup)) == "LightGreen_14")
+  if(length(lightgreen.tmp) > 0){
+    df.out.fixed$LightGreen_14[i] <- mean(as.character(pred.sup$assign[lightgreen.tmp])==as.character(pop(x.sup[lightgreen.tmp])))
+  }
+  print(i)
+}
+
+write.csv(file="analysis/dapc_accuracy_fixed.csv", df.out.fixed, row.names=F)
+
+df_long <- df.out.fixed %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = "population",
+    values_to = "accuracy"
+  )
+
+
+p1 <- ggplot(df_long, aes(x=population, y=accuracy, fill=population))+
+  geom_boxplot()+
+  ylab("% correct assignments") +
+  xlab("population") + 
+  theme_bw(base_size = 14) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+        legend.position="none") +
+  ylim(0,1) +
+  scale_fill_manual(values=c("darkgreen", "lawngreen", "orange3", "red3")) +
+  ggtitle("DAPC: 10 individual training")
+
+ggsave("figures/dapc_fixedIndiv_accuracy.png",p1,
+       h=4, w=5)
+
